cf7b82a1a01ccdc33d57654ee9a15de6e213af27
Raul995 <70592694+Raul995@users.noreply.github.com>
2021-05-21 06:18:46 -0500

    //push final este si

--- app/src/main/java/com/udecar/InicioRegistrado.java
+++ app/src/main/java/com/udecar/InicioRegistrado.java
@@ -9,12 +9,51 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.udecar.Datos.Motor;
+
+import org.w3c.dom.Text;
 
 public class InicioRegistrado extends Fragment {
+
+    private FirebaseAuth auth;
+    private TextView nombreUser;
+    private DatabaseReference dataBase = FirebaseDatabase.getInstance().getReference();
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_inicio_registrado,container,false);
+        auth = FirebaseAuth.getInstance();
+
+        nombreUser = (TextView) view.findViewById(R.id.txt_usuario);
+
+        String id = auth.getUid();
+
+        dataBase.child("Users").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    for(DataSnapshot templateSnapshot : snapshot.getChildren()){
+                        if (templateSnapshot.getKey().equals(id)){
+                            nombreUser.setText((CharSequence) templateSnapshot.child("nombre").getValue());
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         return view;
     }
